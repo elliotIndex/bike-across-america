7ce22e12af62de382f7c5a31fb67548a8c9502ae
Elliot Plant <elliotaplant@gmail.com>
2016-02-15 11:34:42 -0800

    Run server

--- server/server.js
+++ server/server.js
@@ -1,16 +1,16 @@
 var express = require('express');
-var mongoose = require('mongoose');
+// var mongoose = require('mongoose');
 
 var app = express();
 
-mongoose.connect('mongodb://localhost/bikeAcrossAmerica');
+// mongoose.connect('mongodb://localhost/bikeAcrossAmerica');
 
 // // configure our server with all the middleware and routing
 // require('./config/middleware.js')(app, express);
 // require('./config/routes.js')(app, express);
 
 // start listening to requests on port 8000
-app.listen(3000);
+app.listen(8000);
 
 // export our app for testing and flexibility, required by index.js
 module.exports = app;
