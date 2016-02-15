76409d370611f846c283599b3ce83673379484b2
Elliot Plant <elliotaplant@gmail.com>
2016-02-15 11:54:48 -0800

    User error helper functions

--- server/config/routes.js
+++ server/config/routes.js
@@ -15,6 +15,6 @@ module.exports = function (app, express) {
 
   // If a request is sent somewhere other than the routes above,
   // send it through our custom error handler
-  // app.use(helpers.errorLogger);
-  // app.use(helpers.errorHandler);
+  app.use(helpers.errorLogger);
+  app.use(helpers.errorHandler);
 };
