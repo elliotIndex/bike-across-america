17a943ded807255143e2d6f655c913b7f6618f22
Elliot Plant <elliotaplant@gmail.com>
2016-02-15 11:45:47 -0800

    Add middlwear to server and it didn't break

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ var app = express();
 mongoose.connect('mongodb://localhost/bikeAcrossAmerica');
 
 // // configure our server with all the middleware and routing
-// require('./config/middleware.js')(app, express);
+require('./config/middleware.js')(app, express);
 // require('./config/routes.js')(app, express);
 
 // start listening to requests on port 8000
