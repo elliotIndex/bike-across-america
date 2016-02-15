16df86326809eff116d41ad86673802ca984ef1b
Elliot Plant <elliotaplant@gmail.com>
2016-02-15 12:10:27 -0800

    Add test controller to router
    Successfuly reply to get request at root '/'

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,5 +1,6 @@
 // var linksController = require('../links/linkController.js');
 // var userController = require('../users/userController.js');
+var testController = require('../test/testController.js');
 var helpers = require('./helpers.js'); // our custom middleware
 
 module.exports = function (app, express) {
@@ -13,6 +14,7 @@ module.exports = function (app, express) {
   // app.get('/api/links/', linksController.allLinks);
   // app.post('/api/links/', linksController.newLink);
 
+  app.get('/', testController.testFunc);
   // If a request is sent somewhere other than the routes above,
   // send it through our custom error handler
   app.use(helpers.errorLogger);
---
+++ server/test/testController.js
@@ -0,0 +1,6 @@
+
+module.exports = {
+  testFunc: function (req, res, next) {
+    res.json([1, 3, "test"]);
+  }
+};
