ee5fa3dbb06a4e25c566e0105825b174f008b20a
Elliot Plant <elliotaplant@gmail.com>
2016-02-15 12:59:58 -0800

    Add main app page and appropriately format bower.json

---
+++ client/app/services/services.js
@@ -0,0 +1,68 @@
+angular.module('shortly.services', [])
+
+// .factory('Links', function ($http) {
+//
+//   var getAll = function () {
+//     return $http({
+//       method: 'GET',
+//       url: '/api/links'
+//     })
+//     .then(function (resp) {
+//       return resp.data;
+//     });
+//   };
+//
+//   var addOne = function (link) {
+//     return $http({
+//       method: 'POST',
+//       url: '/api/links',
+//       data: link
+//     });
+//   };
+//
+//   return {
+//     getAll: getAll,
+//     addOne: addOne
+//   };
+//   })
+.factory('Auth', function ($http, $location, $window) {
+
+  var signin = function (user) {
+    return $http({
+      method: 'POST',
+      url: '/api/users/signin',
+      data: user
+    })
+    .then(function (resp) {
+      return resp.data.token;
+    });
+  };
+
+  var signup = function (user) {
+    return $http({
+      method: 'POST',
+      url: '/api/users/signup',
+      data: user
+    })
+    .then(function (resp) {
+      return resp.data.token;
+    });
+  };
+
+  var isAuth = function () {
+    return !!$window.localStorage.getItem('com.shortly');
+  };
+
+  var signout = function () {
+    $window.localStorage.removeItem('com.shortly');
+    $location.path('/signin');
+  };
+
+
+  return {
+    signin: signin,
+    signup: signup,
+    isAuth: isAuth,
+    signout: signout
+  };
+});
