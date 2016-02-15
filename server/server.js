08eb9dc767b348f8309ab00d03d8191a6384d743
Elliot Plant <elliotaplant@gmail.com>
2016-02-15 11:49:58 -0800

    Add router to server and it didn't break

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ mongoose.connect('mongodb://localhost/bikeAcrossAmerica');
 
 // // configure our server with all the middleware and routing
 require('./config/middleware.js')(app, express);
-// require('./config/routes.js')(app, express);
+require('./config/routes.js')(app, express);
 
 // start listening to requests on port 8000
 app.listen(8000);
