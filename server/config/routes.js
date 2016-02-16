1b398086e447d2966ca42d0dcdc4ce9a9ea5c97c
Elliot Plant <elliotaplant@gmail.com>
2016-02-15 17:15:01 -0800

    Remove test controller folder and requirements

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,6 +1,5 @@
 // var linksController = require('../links/linkController.js');
 var userController = require('../users/userController.js');
-var testController = require('../test/testController.js');
 var helpers = require('./helpers.js'); // our custom middleware
 
 module.exports = function (app, express) {
@@ -15,7 +14,6 @@ module.exports = function (app, express) {
   // app.get('/api/links/', linksController.allLinks);
   app.post('/api/users/miles', userController.addMiles);
   app.get('/api/users/getUser', userController.getUser);
-  app.get('/', testController.testFunc);
   // If a request is sent somewhere other than the routes above,
   // send it through our custom error handler
   app.use(helpers.errorLogger);
--- server/test/testController.js
+++
@@ -1,6 +0,0 @@
-
-module.exports = {
-  testFunc: function (req, res, next) {
-    res.json([1, 3, "test"]);
-  }
-};
